cdb0c8b367b804736dc96ffbcd9c2206e5d7963f
Matt Secrest <secrestm@gene.com>
2024-01-08 22:09:25 -0800

    wip

--- tests/testthat/test-generate_ccw.R
+++ tests/testthat/test-generate_ccw.R
@@ -1,6 +1,6 @@
 
 test_that("weights are adequately calculated compared to Maringe", {
-  
+
   df <- toy_df |>
     create_clones(id = "id", event = "death", time_to_event = "fup_obs", exposure = "surgery", time_to_exposure = "timetosurgery", ced_window = 182.62) |>
     cast_clones_to_long() |>
@@ -25,15 +25,11 @@ test_that("weights are adequately calculated compared to Maringe", {
     )
   }
 
-  cox_df <- survival::coxph(survival::Surv(t_start, t_stop, outcome) ~ clone, data = df)
-  cox_data_long_cox <- survival::coxph(survival::Surv(t_start, t_stop, outcome) ~ clone, data = data_long_cox)
+  cox_df <- survival::coxph(survival::Surv(t_start, t_stop, outcome) ~ clone, data = df, weights = weight_cox)
+  cox_data_long_cox <- survival::coxph(survival::Surv(t_start, t_stop, outcome) ~ clone, data = data_long_cox, weights = weight_cox)
 
   expect_equal(cox_df$coefficients, cox_data_long_cox$coefficients, tolerance = 1e-6)
 
-  load(system.file("tests/testthat/data/data.long.Cox_maringe.RData", package = "survivalCCW"))
-
-  cox.data.long.Cox <- survival::coxph(survival::Surv(Tstart, Tstop, outcome) ~ arm, data = data.long.Cox)
-  
 })
 
 
