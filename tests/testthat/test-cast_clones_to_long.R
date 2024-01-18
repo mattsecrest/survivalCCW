c3761f98fd1e6ad1d2917c98e708849b54b48428
Matt Secrest <secrestm@gene.com>
2024-01-18 08:06:28 -0800

    wip example clones to long tests

--- tests/testthat/test-cast_clones_to_long.R
+++ tests/testthat/test-cast_clones_to_long.R
@@ -1,4 +1,4 @@
-test_that("casting clones requires ccw_clones class", {
+test_that("appropriate class (ccw_clones) is caught", {
 
   df <- data.frame(
     id = c(1, 2, 3, 4),
@@ -9,7 +9,8 @@ test_that("casting clones requires ccw_clones class", {
   )
 
   expect_error(
-    cast_clones_to_long(df)
+    cast_clones_to_long(df),
+    "Must inherit from class 'ccw_clones', but has class 'data.frame'."
   )
 
   ccw_df <- create_clones(df, id = "id", event = "event", time_to_event = "time_to_event", exposure = "exposure", time_to_exposure = "time_to_exposure", ced_window = 200)
@@ -17,24 +18,102 @@ test_that("casting clones requires ccw_clones class", {
   attributes(ccw_df)$id <- NULL
 
   expect_error(
-    cast_clones_to_long(ccw_df)
+    cast_clones_to_long(ccw_df),
+    "The input data.frame is missing at least one attribute"
   )
 
   ccw_df$outcome <- NULL
 
   expect_error(
-    cast_clones_to_long(ccw_df)
+    cast_clones_to_long(ccw_df),
+    "The input data.frame is missing at least one of the required columns"
   )
+
 })
 
 test_that("long format was created correctly", {
 
-  df_long <- toy_df |>
-    create_clones(id = "id", event = "death", time_to_event = "fup_obs", exposure = "surgery", time_to_exposure = "timetosurgery", ced_window = 182.62) |>
-    cast_clones_to_long()
+  # Check statuses for all combinations:
+  # Exposed before CED vs unexposed before CED 
+  # Censored before CED vs censored after CED
+  # Death before CED vs death after CED
 
-  expect_true(TRUE)
-  #@TODO more test cases
+  ced <- 100
+  exposure_time <- c(80, NA_real_)
+  fup_time <- c(85, 125)
+  event <- c(0L, 1L)
+
+  df <- expand.grid(
+    exposure_time = exposure_time,
+    fup_time = fup_time,
+    event = event
+  )
+
+  df$exposure <- !is.na(df$exposure_time)
+  df$id <- 1:NROW(df)
+
+  ccw_df <- create_clones(
+    df, 
+    id = "id", 
+    event = "event", 
+    time_to_event = "fup_time", 
+    exposure = "exposure", 
+    time_to_exposure = "exposure_time", 
+    ced_window = ced
+  )
+
+  df_long <- cast_clones_to_long(ccw_df)
+
+  ## Truly exposed
+  ### Exposed clone
+  df_long_ <- df_long[df_long$id %in% df[df$exposure==1, "id"] & df_long$clone == 1, ]
+  df_long_$id_time <- paste0(df_long_$id, "_", df_long_$time_id)
+  df_max_time_id <- aggregate(time_id ~ id, data = df_long_, max)
+  df_long_last <- merge(df_long_, df_max_time_id, by = c("id", "time_id"))
+  df_long_other <- df_long_[!df_long_$id_time %in% df_long_last$id_time,]
+  
+  expect_equal(df_long_last$outcome, df[df$exposure==1, "event"])
+  expect_true(all(df_long_other$outcome == 0))
+  expect_equal(df_long_last$t_stop, df[df$exposure==1, "fup_time"])
+  expect_equal(df_long_last$outcome, df[df$exposure==1, "event"])
+  expect_true(all(df_long_$censor ==0))
+
+  ### Unexposed clone
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 1L, "id"] & ccw_df$clone == 0L, "outcome"] == 0))
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 1L, "id"] & ccw_df$clone == 0L, "fup_outcome"], df[df$exposure == 1L, "exposure_time"])
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 1L, "id"] & ccw_df$clone == 0L, "censor"] == 1L))
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 1L, "id"] & ccw_df$clone == 0L, "fup_censor"], df[df$exposure == 1L, "exposure_time"])
+
+  ## Truly unexposed
+  ### Censor before CED 
+  #### Exposed clone
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 1L, "outcome"], df[df$exposure == 0L & df$fup_time < ced, "event"])
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 1L, "fup_outcome"], df[df$exposure == 0L & df$fup_time < ced, "fup_time"])
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 1L, "censor"] == 0L))
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 1L, "fup_censor"], df[df$exposure == 0L & df$fup_time < ced, "fup_time"])
+
+  #### Unexposed clone
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 0L, "outcome"], df[df$exposure == 0L & df$fup_time < ced, "event"])
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 0L, "fup_outcome"], df[df$exposure == 0L & df$fup_time < ced, "fup_time"])
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 0L, "censor"] == 0L))
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time < ced, "id"] & ccw_df$clone == 0L, "fup_censor"], df[df$exposure == 0L & df$fup_time < ced, "fup_time"])
+
+  ### Censor after CED
+  #### Exposed clone
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 1L, "outcome"] == 0))
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 1L, "fup_outcome"] == ced))
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 1L, "censor"] == 1L))
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 1L, "fup_censor"] == ced))
+
+  #### Unexposed clone
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 0L, "outcome"], df[df$exposure == 0L & df$fup_time >= ced, "event"])
+  expect_equal(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 0L, "fup_outcome"], df[df$exposure == 0L & df$fup_time >= ced, "fup_time"])
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 0L, "censor"] == 0L))
+  expect_true(all(ccw_df[ccw_df$id %in% df[df$exposure == 0L & df$fup_time >= ced, "id"] & ccw_df$clone == 0L, "fup_censor"] == ced))
+
+
+  
+  
 })
 
 
