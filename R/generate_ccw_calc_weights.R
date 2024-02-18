84af7f03accf80c6a7c7e366aab9462bd8d3243d
Matt Secrest <secrestm@gene.com>
2024-02-18 06:42:54 -0800

    updated weighting function

--- R/generate_ccw_calc_weights.R
+++ R/generate_ccw_calc_weights.R
@@ -46,7 +46,7 @@ generate_ccw_calc_weights <- function(df,  event_times_df, predvars) {
       all.x = TRUE
    )
 
-   df <- df[order(df$id,df$fup_outcome),]
+   df <- df[order(df[,id], df$fup_outcome),]
    df$hazard <- ifelse(is.na(df$hazard), 0, df$hazard)
    df$p_uncens <- exp(-(df$hazard) * exp(df$lp))
    df$weight_cox  <- 1 / df$p_uncens
